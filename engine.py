0284a58275fe921ad5bf5e9cbf53c578fcf56348
Creonox <Nathan_K@hotmail.de>
2018-08-05 13:17:18 +0200

    added render file

--- engine.py
+++ engine.py
@@ -1,13 +1,16 @@
 import libtcodpy as libtcod
 
+from entity import Entity
 from input_handlers import handle_keys
+from render_functions import clear_all, render_all
 
 def main():
 	screen_width = 60
 	screen_height = 35
 	
-	player_x = int(screen_width / 2)
-	player_y = int(screen_height / 2)
+	player = Entity(int(screen_width / 2), int(screen_height / 2), '@', libtcod.white)
+	npc = Entity(int(screen_width / 2 + 5), int(screen_height / 2), '@', libtcod.yellow)
+	entities = [npc, player]
 	
 	libtcod.console_set_custom_font('dejavu_wide16x16_gs_tc.png', libtcod.FONT_TYPE_GREYSCALE | libtcod.FONT_LAYOUT_TCOD)
 	
@@ -21,13 +24,11 @@ def main():
 	while not libtcod.console_is_window_closed():
 		libtcod.sys_check_for_event(libtcod.EVENT_KEY_PRESS, key, mouse)
 		
-		libtcod.console_set_default_foreground(con, libtcod.white)
-		libtcod.console_put_char(con, player_x, player_y, '@', libtcod.BKGND_NONE)
-		libtcod.console_blit(con, 0, 0, screen_width, screen_height, 0, 0, 0)
+		render_all(con, entities, screen_width, screen_height)
 		libtcod.console_flush()
-		
-		libtcod.console_put_char(con, player_x, player_y, ' ', libtcod.BKGND_NONE)
-		
+				
+		clear_all(con,entities)
+				
 		action = handle_keys(key)
 		
 		move = action.get('move')
@@ -36,8 +37,7 @@ def main():
 		
 		if move:
 			dx, dy = move
-			player_x += dx
-			player_y += dy
+			player.move(dx, dy)
 		
 		if exit:
 			return True
